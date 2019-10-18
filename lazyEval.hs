0bf37bbaa6e09a46f3792a6412010c5bfb27ad9a
Dave Nathanael <davenathanaeldev@gmail.com>
2019-10-18 20:54:16 +0700

    Added lazy eval and list comprehension exercise problems

---
+++ lazyEval.hs
@@ -0,0 +1,8 @@
+-- 1. Uraikan langkah evaluasi dari ekspresi berikut: [ x+y | x <- [1 .. 4], y <- [2 .. 4], x > y ]
+-- 2. Buatlah fungsi divisor yang menerima sebuah bilangan bulat n dan mengembalikan
+--    list bilangan bulat positif yang membagi habis n
+-- 3. Buatlah definisi quick sort menggunakan list comprehension.
+-- 4. Buatlah definisi infinite list untuk permutation.
+-- 5. Buatlah definisi untuk memberikan infinite list dari bilangan prima menerapkan algoritma Sieve of Erastothenes.
+-- 6. Buatlah definisi infinite list dari triple pythagoras. List tersebut terdiri dari element
+--    triple bilangan bulat positif yang mengikut persamaan pythagoras x^2 + y^2 = z^2
