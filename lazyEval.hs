116bc02e801bb692cfcc5eb64d48e0b98e91581b
Dave Nathanael <davenathanaeldev@gmail.com>
2019-10-18 21:21:28 +0700

    Add quicksort impl

--- lazyEval.hs
+++ lazyEval.hs
@@ -9,6 +9,9 @@
 divisor :: Integral a => a -> [a]
 divisor n = [x | x <- [1..n], n `mod` x == 0]
 -- 3. Buatlah definisi quick sort menggunakan list comprehension.
+-- quickSort :: Ord a => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) = (quickSort [y | y <- xs, y <= x]) ++ [x] ++ (quickSort [y | y <- xs, y > x])
 -- 4. Buatlah definisi infinite list untuk permutation.
 -- 5. Buatlah definisi untuk memberikan infinite list dari bilangan prima menerapkan algoritma Sieve of Erastothenes.
 -- 6. Buatlah definisi infinite list dari triple pythagoras. List tersebut terdiri dari element
