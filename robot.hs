1e72b414ba989a6b4fa362844e7eaf4d95714105
Dave Nathanael <davenathanaeldev@gmail.com>
2019-10-18 22:39:43 +0700

    Add robot update state

--- robot.hs
+++ robot.hs
@@ -5,9 +5,10 @@ import Data.List
 import Control.Monad
 import Control.Applicative
 import System.IO
--- import SOE
 
 type Position = (Int,Int)
+
+type Grid = Array Position [Direction]
 
 data Direction = North | Ease | South | West
       deriving (Eq,Show,Enum)
@@ -27,7 +28,8 @@ data RobotState
          }
       deriving Show
 
-newtype Robot = Robot RobotState
+newtype Robot a
+  = Robot (RobotState)
 
 right :: Direction -> Direction
 right d = toEnum $ succ (fromEnum d) `mod` 4
@@ -35,3 +37,14 @@ right d = toEnum $ succ (fromEnum d) `mod` 4
 left :: Direction -> Direction
 left d = toEnum $ pred (fromEnum d) `mod` 4
 
+updateState :: (RobotState -> RobotState) -> Robot
+updateState u = Robot (\s -> return (u s, ()))
+
+-- turnLeft :: Robot
+-- turnLeft = updateState (\s -> s {facing=left (facing s)})
+
+-- turnRight :: Robot
+-- turnRight = updateState (\s -> s {facing=right (facing s)})
+
+-- testFunc :: RobotState -> RobotState
+-- testFunc = (\s -> s {facing=right (facing s)})
