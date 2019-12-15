0cca22e6dbda889c706a841d7e8a6ea6514b2bc1
Dave Nathanael <davenathanaeldev@gmail.com>
2019-12-15 18:55:53 +0700

    Added various type of last

--- quizzes/quiz2.hs
+++ quizzes/quiz2.hs
@@ -6,5 +6,14 @@ rotabc (x:str)
   | x == 'c' = 'a' : rotabc str
   | otherwise = x : rotabc str
 
+-- last with pointfree style
 last :: [a] -> a
 last = head . reverse
+
+-- last with foldl
+lastWithFoldl :: [a] -> a
+lastWithFoldl = foldl (\x a -> a) 0
+
+-- last with foldr
+lastWithFoldr :: [a] -> a
+lastWithFoldr = foldr (\x a -> x) 0
